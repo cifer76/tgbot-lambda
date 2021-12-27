619be6d480ef9b7ac28450831262da58b4d3cfa8
cifer <mantianyu@gmail.com>
2021-12-27 14:33:19 +0800

    reverse sorting

--- dto.go
+++ dto.go
@@ -38,7 +38,7 @@ type GroupRecord struct {
 type GroupRecords []GroupRecord
 
 func (a GroupRecords) Len() int           { return len(a) }
-func (a GroupRecords) Less(i, j int) bool { return a[i].MemberCount < a[j].MemberCount }
+func (a GroupRecords) Less(i, j int) bool { return a[i].MemberCount > a[j].MemberCount }
 func (a GroupRecords) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 
 // Tag Record
